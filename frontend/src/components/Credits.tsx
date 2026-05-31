7ed3657bc79d1f77e1bd157a273cbf4766ad41d7
chrislovespython <nishsimone777@gmail.com>
2026-05-31 15:53:55 +0000

    credits

--- frontend/src/components/Credits.tsx
+++ frontend/src/components/Credits.tsx
@@ -1,8 +1,6 @@
 export function Credits() {
   return (
-    <footer className="mt-10 border-t border-white/10 pt-5 text-xs leading-5 text-zinc-600 flex items-center">
-      <p className="flex items-center w-96 gap-1">Made by <a href="https://x.com/2christaken" target="_blank" rel="noopener noreferrer" className="text-lastfm hover:text-red-300">Chris</a></p>
-      <p className="w-full text-end">Liscuss uses Last.fm for listening data.</p>
-    </footer>
+    <footer className="mt-10 border-t border-white/10 pt-5 text-xs leading-5 text-zinc-600 flex items-center justify-center">
+      <p className="flex items-center w-96 gap-1">Made by <a href="https://x.com/2christaken" target="_blank" rel="noopener noreferrer" className="text-lastfm hover:text-red-300">Chris</a></p>    </footer>
   );
 }
