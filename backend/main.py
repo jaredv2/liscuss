ec57414ce91e053b5b1b9eebb71fc0d0b91321f1
chrislovespython <nishsimone777@gmail.com>
2026-05-31 15:54:59 +0000

    fixed cors

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,7 @@ app = FastAPI(title="Liscuss API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["https://liscuss.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
